9523d2fd21d5c2a0aa368b8b31f74ae41b2ace8a
emoryfisher <70530177+emoryfisher@users.noreply.github.com>
2020-09-06 22:25:38 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+var prompter = prompt("Bill?");
+var number1= prompt(number1*.07);
+var number2= prompt(answer1*.05);
+var answer=answer1+answer2+answer3
+document.body.querySelector("#awesome").innerHTML=answer3;
